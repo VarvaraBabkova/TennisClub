ddf699244fd835141b91eb48e423eca060a87739
Varvara Babkova <varvara.munday@gmail.com>
2019-07-31 14:33:07 -0500

    results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,13 @@
 class ResultsController < ApplicationController
+
+	def index
+		@teams = Team.all
+		@games  = Game.all
+		@players = Player.all
+	end
+
+	def show
+		@teams = Team.all
+
+	end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -10,4 +10,33 @@ class Game < ApplicationRecord
         players.each {|p| teams  << p.team.team}
         teams.uniq!
     end
+
+    def get_winner_team
+    	p = self.participations.max {|a, b| a.score <=> b.score}
+    	return p.player.team.team if p.score
+    	return nil if !p.score
+    end
+
+    def get_winner_score
+    	p = self.participations.max {|a, b| a.score <=> b.score}
+    	return p.score if p.score
+    	return nil if !p.score
+    end
+
+    def get_loser_team
+    	p = self.participations.min {|a, b| a.score <=> b.score}
+    	return p.player.team.team if p.score
+    	return nil if !p.score
+    end
+
+    def get_loser_score
+    	p = self.participations.min {|a, b| a.score <=> b.score}
+    	return p.score if p.score
+    	return nil if !p.score
+    end
+
+    def played?
+    	self.participations.first.score
+    end
+
 end
